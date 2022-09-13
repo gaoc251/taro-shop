4d8d083efde9127f5c2990633df8fba4a0af5eb3
gaochong01 <gaochong01@58.com>
2022-09-13 17:25:28 +0800

    add tabbar

--- src/app.config.ts
+++ src/app.config.ts
@@ -8,5 +8,22 @@ export default {
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
-  }
+  },
+  tabBar: {
+    color: 'rgba(0, 0, 0, 0.6)',
+    selectedColor: '#0281ff',
+    backgroundColor: '#fff',
+    borderStyle: 'white',
+    list: [{
+      pagePath: 'pages/index/index',
+      text: '首页',
+      iconPath: "./asset/image/home.png",
+      selectedIconPath: "./asset/image/home_selected.png",
+    }, {
+      pagePath: 'pages/user/index',
+      text: '我的',
+      iconPath: "./asset/image/user.png",
+      selectedIconPath: "./asset/image/user_selected.png",
+    }]
+  },
 }
