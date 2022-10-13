6fc32f2997015e40417f6967b86de05758d0146d
gaochong01 <gaochong01@58.com>
2022-10-13 17:51:12 +0800

    mod

--- src/mockData/homeIcon.js
+++ src/mockData/homeIcon.js
@@ -3,29 +3,34 @@ export default {
     message: 'ok',
     data: {
         icons: [{
-            text: "二手市场",
-                "imgUrl": "https://pic6.ajkimg.com/news/55985a5eaa5170f5f9b2ca15709856c0?imageMogr2/format/jpg/thumbnail/132x132",
-                "jumpAction": "../../pages/infoList/index?type=erMarket"
-            }, {
-                "text": "招聘求职",
-                "imgUrl": "https://pic6.ajkimg.com/news/55985a5eaa5170f5f9b2ca15709856c0?imageMogr2/format/jpg/thumbnail/132x132",
-                "jumpAction": "../../pages/infoList/index?type=jop"
-            }, {
-                "text": "商家动态",
-                "imgUrl": "https://pic6.ajkimg.com/news/55985a5eaa5170f5f9b2ca15709856c0?imageMogr2/format/jpg/thumbnail/132x132",
-                "jumpAction": "../../pages/infoList/index?type=shopTrends"
-            }, {
                 "text": "房屋出租",
                 "imgUrl": "https://pic6.ajkimg.com/news/55985a5eaa5170f5f9b2ca15709856c0?imageMogr2/format/jpg/thumbnail/132x132",
-                "jumpAction": "../../pages/infoList/index?type=houseRent"
+                "jumpAction": "../../pages/infoList/index?type=houseRent",
+                bgColor: '#01A7F0'
+            }, {
+            text: "二手市场",
+                "imgUrl": "https://pic6.ajkimg.com/news/55985a5eaa5170f5f9b2ca15709856c0?imageMogr2/format/jpg/thumbnail/132x132",
+                "jumpAction": "../../pages/infoList/index?type=erMarket",
+                
             }, {
                 "text": "小区问答",
                 "imgUrl": "https://pic6.ajkimg.com/news/55985a5eaa5170f5f9b2ca15709856c0?imageMogr2/format/jpg/thumbnail/132x132",
                 "jumpAction": "../../pages/infoList/index?type=xiaoquQuestions"
             }, {
+                "text": "招聘求职",
+                "imgUrl": "https://pic6.ajkimg.com/news/55985a5eaa5170f5f9b2ca15709856c0?imageMogr2/format/jpg/thumbnail/132x132",
+                "jumpAction": "../../pages/infoList/index?type=jop",
+                bgColor: '#E97E48'
+            },  {
+                "text": "商家动态",
+                "imgUrl": "https://pic6.ajkimg.com/news/55985a5eaa5170f5f9b2ca15709856c0?imageMogr2/format/jpg/thumbnail/132x132",
+                "jumpAction": "../../pages/infoList/index?type=shopTrends",
+                bgColor: '#C849CE'
+            }, {
                 "text": "联系我们",
                 "imgUrl": "https://pic6.ajkimg.com/news/55985a5eaa5170f5f9b2ca15709856c0?imageMogr2/format/jpg/thumbnail/132x132",
-                "jumpAction": "../../pages/infoList/index?type=contactUs"
+                "jumpAction": "../../pages/infoList/index?type=contactUs",
+                bgColor: '#A3C869'
             }],
         headImages: [
             'https://i.postimg.cc/qM9yJXzk/image.png'
