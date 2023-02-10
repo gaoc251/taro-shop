87faf31401e83df1eb93252f81658fc732cc3bb8
gaochong01 <gaochong01@58.com>
2023-02-10 10:42:43 +0800

    mod

---
+++ src/mockData/homeIconConfig.js
@@ -0,0 +1,68 @@
+export default {
+    code: 0,
+    msg: "success",
+    data: {
+        iconConfig: [
+            {
+                "picUrl": "https://yanxuan.nosdn.127.net/c6fd8835a6400b7da7a016ad85506b69.png",
+                "schemeUrl": "https://m.you.163.com/item/newItem",
+                "text": "新品首发",
+                "textColor": "333333"
+            },
+            {
+                "picUrl": "https://yanxuan.nosdn.127.net/fede8b110c502ec5799702d5ec824792.png",
+                "schemeUrl": "https://m.you.163.com/item/list?categoryId=1005000&style=pd",
+                "text": "居家生活",
+                "textColor": "333333"
+            },
+            {
+                "picUrl": "https://yanxuan.nosdn.127.net/896a3beac514ae8f40aafe028e5fec56.png",
+                "schemeUrl": "https://m.you.163.com/item/list?categoryId=1010000&style=pd",
+                "text": "服饰鞋包",
+                "textColor": "333333"
+            },
+            {
+                "picUrl": "https://yanxuan.nosdn.127.net/37520d1204a0c55474021b43dac2a69e.png",
+                "schemeUrl": "https://m.you.163.com/item/list?categoryId=1005002&style=pd",
+                "text": "美食酒水",
+                "textColor": "333333"
+            },
+            {
+                "picUrl": "https://yanxuan.nosdn.127.net/6c3bd9d885c818b1f73e497335a68b47.png",
+                "schemeUrl": "https://m.you.163.com/item/list?categoryId=1013001&style=pd",
+                "text": "个护清洁",
+                "textColor": "333333"
+            },
+            {
+                "picUrl": "https://yanxuan.nosdn.127.net/559d2a240ec20b096590a902217009ff.png",
+                "schemeUrl": "https://m.you.163.com/item/list?categoryId=1011000&style=pd",
+                "text": "母婴亲子",
+                "textColor": "333333"
+            },
+            {
+                "picUrl": "https://yanxuan.nosdn.127.net/5c088559ebcc3f0ffcda663f04dfbeb2.png",
+                "schemeUrl": "https://m.you.163.com/item/list?categoryId=109243029&style=pd",
+                "text": "运动旅行",
+                "textColor": "333333"
+            },
+            {
+                "picUrl": "https://yanxuan.nosdn.127.net/fbca8e1f2948f0c09fc7672c2c125384.png",
+                "schemeUrl": "https://m.you.163.com/item/list?categoryId=1043000&style=pd",
+                "text": "数码家电",
+                "textColor": "333333"
+            },
+            {
+                "picUrl": "https://yanxuan.nosdn.127.net/static-union/1642506913355bb9.png",
+                "schemeUrl": "https://m.you.163.com/item/list?categoryId=109320005&style=pd",
+                "text": "宠物生活",
+                "textColor": "333333"
+            },
+            {
+                "picUrl": "https://yanxuan.nosdn.127.net/static-union/16644541382b06e4.png",
+                "schemeUrl": "https://act.you.163.com/act/pub/ssr/x3o4kGIbPc08.html?appConfig=1_1_1",
+                "text": "每日抄底",
+                "textColor": "333333"
+            }
+        ],
+    }
+}
